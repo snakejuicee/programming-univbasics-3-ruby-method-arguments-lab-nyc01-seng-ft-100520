62a6cb5f460f369e7bb83ab598a07a5f06dc21e4
Patricia Arnedo <paaarnedo@gmail.com>
2020-09-14 20:03:45 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,8 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
+$name = "Josh"
+
 def introduction(name)
   "Hi, my name is #{name}."
 end
@@ -15,7 +17,7 @@ def introduction_with_language_optional(name, language="Ruby")
   "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction "Josh"
+
 
 
 
